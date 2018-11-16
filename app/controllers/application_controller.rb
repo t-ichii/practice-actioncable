cac0ddbf728b43f4ff391652d73ca1ad0a3f3475
Takeru Ichii <t.ichii@c-fo.com>
2018-11-16 13:17:41 +0900

    can launch rails server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :current_user
+
+  def current_user
+    @current_user ||= User.find(cookies[:user_id])
+  rescue ActiveRecord::RecordNotFound => _e
+    @current_user = User.create!
+    cookies.permanent[:user_id] = @current_user.id
+  end
 end
